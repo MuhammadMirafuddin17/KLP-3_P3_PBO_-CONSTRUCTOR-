f2ec40ce9de8118cb9e36f652a4da465e7ebdd5d
Ern <148302079+Ernawatiiii@users.noreply.github.com>
2023-10-26 00:48:25 +0800

    Add files via upload

---
+++ Jobshet_Ernawati/p3nomor1_F1B021040.java
@@ -0,0 +1,15 @@
+class Scincare{
+    String produk;
+    int harga;
+}
+public class p3nomor1_F1B021040 {
+   public static void main (String[] args){
+    Scincare newScin = new Scincare();
+
+    newScin.produk = "nivea";
+    newScin.harga = 21;
+
+    System.out.println(newScin.produk);
+    System.out.println(newScin.harga);
+} 
+}
